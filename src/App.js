1651f7c2afc642d0ecd604a3d5e93946e1ced800
Joshua Yang <85262856+joshuayanggithub@users.noreply.github.com>
2023-02-06 03:46:50 -0800

    Layout

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Contacts from "./components/Contacts";
 import Setup from "./components/Setup";
-import Projects from "./components/Projects";
 import About from './components/About';
+import Goals from "./components/Goals";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
-      <Projects />
       <About />
+      <Goals />
       <Setup />
       <Contacts />
     </>
