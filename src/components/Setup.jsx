d705cf722be47e7d4eab124cc47f389ea29de3d5
Joshua Yang <85262856+joshuayanggithub@users.noreply.github.com>
2023-02-07 01:17:09 -0800

    Preliminary Setup Page added

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Setup = () => {
   return (
-    <div>
-
+    <div class="md:flex justify-center items-center">
+      <p class="p-10 font-pageheader items-center text-center"> I think I have a nice setup and deskplace for productivity. Ofc, the reality is it is messy most of the time and not organized at all</p>
+      <div class=" h-1/4 w-1/3 m-10">
+        <h1 class="text-2xl">Setup</h1>
+        <img src="/imgs/Desk.jpg" class=""/>
+      </div>
     </div>
   );
 }
