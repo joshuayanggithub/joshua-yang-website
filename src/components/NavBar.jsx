26ee83fa24f63cffc76882733d5c1ba622a06de8
Joshua Yang <85262856+joshuayanggithub@users.noreply.github.com>
2023-02-08 07:03:02 -0800

    Mail Button Added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,25 +29,27 @@ const NavBar = () => {
     <div class="fixed w-full flex items-center"> {/*md:flex flex-row w-full justify-between items-center mx-auto */}
       
       <div class="md:flex align-center my-0 mx-auto justify-between items-center w-full p-5">
-        <a href="#Home" class="align-center duration-300 font-pageheader text-black hover:font-bold hover:text-gray-500 text-3xl p-5">Joshua Yang.</a>
+        <a href="#Home" class="align-center duration-300 font-pageheader text-black hover:font-bold hover:text-gray-200 text-3xl p-5">Joshua Yang.</a>
         {/*rounded-full transition-shadow ease-in-out duration-300 shadow-none hover:shadow-2xl*/}
         <div class="p-5 md:flex gap-8 items-center">
           <ul class="hidden md:flex gap-10">
             {
               links.map((link) => 
                 <li key={link.id} class="">
-                  <a href={'#' + link.name} class="scroll-smooth text-2xl text-black p-3 hover:font-bold hover:text-gray-500 duration-300 font-navheader">{link.name}</a>
+                  <a href={'#' + link.name} class="scroll-smooth text-2xl text-black p-3 hover:font-bold hover:text-gray-200 duration-300 font-navheader">{link.name}</a>
                 </li>
               )
             }
           </ul>
-          <div class="hidden md:flex hover:text-bold hover:text-gray-500">
-            <IconContext.Provider value={{ className: "shared-class", size: 40, fontWeight: "bold", }}> {/*Why do I have to use IconContext to work nvm???*/}
-              <>
-                <BsMailbox2 /> 
-              </>
-            </IconContext.Provider>
-          </div>
+          <a href="mailto: jyangftw@gmail.com">
+            <div class="hidden md:flex hover:text-bold hover:text-gray-500">
+              <IconContext.Provider value={{ className: "shared-class", size: 40, fontWeight: "bold", }}> {/*Why do I have to use IconContext to work nvm???*/}
+                <>
+                  <BsMailbox2 /> 
+                </>
+              </IconContext.Provider>
+            </div>
+          </a>
         </div>
       </div>
 
@@ -55,7 +57,7 @@ const NavBar = () => {
         {nav ? menuButton: closeButton}
       </div>
 
-      <div class={!nav ? 'fixed left-0 top-0 w-[30%] h-full border-r bg-gray-500 bg-opacity-80 border-r-gray-600 ease-in-out duration-500' : "fixed  ease-in-out duration-500 left-[-100%] "}>
+      <div class={!nav ? 'fixed left-0 top-0 w-[30%] h-full border-r bg-gray-500 bg-opacity-80 border-r-gray-200 ease-in-out duration-500' : "fixed  ease-in-out duration-500 left-[-100%] "}>
         <h1 class="font-pageheader text-5xl font-bold text-black m-10">JY</h1>
         <ul class="p-4">
           {
