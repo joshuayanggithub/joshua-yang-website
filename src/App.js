878bd9006106b29ac668ff03b3591c85a9ceb4b0
Joshua Yang <jyangftw@gmail.com>
2023-02-12 14:00:13 -0800

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (<>
-    <div id="Footer">
-
+    <div id="Footer" class="bg-gray-100 pt-20 pb-5 grid place-items-center">
+      <h1 class="text-xs w-11/12 ">©2022-2023 Licensed By Joshua Yang</h1>
     </div>
-
   </>)
 }
 
