7e7a1e4924ff1f179554022f5f9eb7464deec6b9
Joshua Yang <jyangftw@gmail.com>
2023-02-12 11:16:29 -0800

    Blind Animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,31 @@
 import React from "react"; 
-import {useParallax } from 'react-scroll-parallax';
+import {useParallax,Parallax } from 'react-scroll-parallax';
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const About = () => {
+  const hiddenMask = 'repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)';
+  const visibleMask = 'repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)';
+  const [inView, setInView] = useState(false);
+
   return  (
     <>
+        <motion.div class="flex justify-center my-40"
+          initial = {false}
+          animate={
+            inView
+              ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+              : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+          }
+          transition={{ duration: 0.9, delay: 0.5 }}
+          onViewportEnter={() => setInView(true)}
+          onViewportLeave={() => setInView(false)}
+        >
+          <img src="/imgs/sunset3.jpg" class = "w-11/12 "/>
+        </motion.div>
+
         <div id="About" class = "flex">
+
           <div class="left-0 flex-col">
             <motion.h1 class="p-1 m-4 uppercase text-8xl font-bold font-fancyheader"
             whileInView={{x:100}}
