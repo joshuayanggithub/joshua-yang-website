67c843bba998b228615155dc9a37917f39b59312
Joshua Yang <jyangftw@gmail.com>
2023-04-24 17:25:02 -0700

    Redo .gitignore cached files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//LOADING ANIMATION
 const loaderText = document.querySelector(".loader");
 
 createBannerSpans(loaderText)
@@ -17,13 +18,13 @@ function createBannerSpans(querySelector) {
 }
 
 let index = 0;
-const seconds = 30;
+const seconds = 100;
 const decreasingInterval = []
 for (let i=0; i<11; ++i) {
   decreasingInterval.push(seconds + Math.log2((i+1))*seconds );
 }
 console.log(decreasingInterval);
-timer = setInterval(animateMergeInIndividual, 20, loaderText);
+timer = setInterval(animateFadeInIndividual, seconds, loaderText, 11);
 
 // function animateMergeInGroup(querySelector) {
 //   index = 0;
@@ -32,13 +33,25 @@ timer = setInterval(animateMergeInIndividual, 20, loaderText);
 //   timer = setInterval(animateMergeInIndividual, seconds, querySelector);
 // }
 
-function animateMergeInIndividual(querySelector) {
+function animateFadeInIndividual(querySelector, stopLength) {
   const span = querySelector.querySelectorAll('span')[index];
   console.log(typeof(span));
   console.log(span);
   span.classList.add("fade");
   ++index;
-  if (index == 11) {
+  if (index == stopLength) {
+    clearInterval(timer);
+    timer = null;
+  }
+}
+
+function animateMergeInIndividual(querySelector, stopLength) {
+  const span = querySelector.querySelectorAll('span')[index];
+  console.log(typeof(span));
+  console.log(span);
+  span.classList.add("merged");
+  ++index;
+  if (index == stopLength) {
     clearInterval(timer);
     timer = null;
   }
@@ -50,26 +63,47 @@ function removeLoader() {
 }
 
 setTimeout(removeLoader, 1500);
+setTimeout(animateBody, 1500);
 
 //After Initial Load-In
 
 const logo = document.querySelector(".logo");
 const title = document.querySelector(".title");
+const navBar = document.querySelector(".menu");
 
+function animateBody() {
+  index = 0;
+  createBannerSpans(title);
+  console.log(title);
+  timer = setInterval(animateMergeInIndividual, 20, title, 11);
+
+}
 
 window.addEventListener('scroll',trackScroll);
 
 function trackScroll() {
   let documentHeight = document.documentElement.scrollHeight; //total document height
-  let vpOffset = window.innerHeight; //viewport height
-  let elementOffset = document.querySelector('.scroll-tracker').offsetHeight; //element height
+  let documentWidth = document.documentElement.scrollWidth; 
+
+  let vpHeight = window.innerHeight; //viewport height
+  let vpWidth = window.innerHeight; //viewport width
+
+  let elementHeight = document.querySelector('.scroll-tracker').offsetHeight; //element height
+  let elementWidth = document.querySelector('.scroll-tracker').offsetWidth; //element width
+
   let curY = window.scrollY; //only gets top of vp height so we get offset
 
-  // console.log(curY + " " + (vpOffset) + " " + documentHeight);
-  let percentScroll = ((curY/(documentHeight-vpOffset)));
-  let pixelScroll = percentScroll*vpOffset;
-  let pixelScrollMax = vpOffset - elementOffset; //so I can still see the bottom of the tracker
-  let finalTrackValue = Math.min(pixelScroll,pixelScrollMax);
-  console.log(finalTrackValue);
-  document.querySelector('.scroll-tracker').style["top"] =  finalTrackValue + "px";
+  console.log(curY + " " + (vpHeight) + " " + documentHeight);
+  let percentScrolled = ((curY/(documentHeight-vpHeight)));
+
+  let pixelScrolledVertical = percentScrolled*vpHeight;
+  let pixelScrolledVerticalMax = vpHeight - elementHeight; //so I can still see the bottom of the tracker
+
+  let finalTrackerHeight = Math.min(pixelScrolledVertical,pixelScrolledVerticalMax);
+
+  console.log(finalTrackerHeight);
+  document.querySelectorAll('.scroll-tracker')[0].style["top"] = finalTrackerHeight + "px";
+  document.querySelectorAll('.scroll-tracker')[1].style["left"] = finalTrackValue + "px";
+  document.querySelectorAll('.scroll-tracker')[2].style["top"] = finalTrackerHeight + "px";
+
 }
