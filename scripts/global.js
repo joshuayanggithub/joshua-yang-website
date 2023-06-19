e784e037131740d2aa5fe28a3b1f3c0dc758bdc0
Joshua Yang <jyangftw@gmail.com>
2023-06-18 21:21:53 -0500

    Menu Eases!

--- scripts/global.js
+++ scripts/global.js
@@ -10,18 +10,48 @@ const aside = document.querySelector("aside");
 const burger = document.querySelector(".hamburger");
 let opened = false;
 
+let myFunc = () => {
+    console.log("Done");
+}
+
+const menuOpenClose = gsap.timeline(
+    {
+        paused: true,
+        onComplete: myFunc,
+    }
+);
+
+menuOpenClose.from(aside,
+    {
+        height: '0%',
+        duration: 0.5,
+    }
+);
+menuOpenClose.from(
+    ".menu-column a", 
+{
+    yPercent: '100',
+    // ease: CustomEase.create("easeName", "0.86,0.13,0.404,0.773"),
+    ease: Expo.easeInOut,
+    duration: '1.2',
+    stagger: '0.1',
+}
+)
+
 burger.addEventListener('click', () => {
   if (!opened) {
     burger.classList.add("open");
-    // footer.classList.add("open");
-    aside.classList.add("open");
+    // // footer.classList.add("open");
+    // aside.classList.add("open");
     opened = true;
+    menuOpenClose.play();
   }
   else {
     burger.classList.remove("open");
     // footer.classList.remove("open");
-    aside.classList.remove("open");
+    // aside.classList.remove("open");
     opened = false;
+    menuOpenClose.reverse();
   }
 })
 
